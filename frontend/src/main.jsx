670a8b42f62fbae71e001cde53a2bcb58c799bb9
riteshk-7600 <riteshk7600@gmail.com>
2025-12-30 16:14:42 +0530

    Auto-sync: 2025-12-30 16:14:42

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import axios from 'axios'
+
+// Set axios base URL relative to current domain
+axios.defaults.baseURL = window.location.origin
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
